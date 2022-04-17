cbcf9a15d9fc504e90398028ba29cdc262a4a243
gans32 <amick.abigail@gmail.com>
2022-04-17 13:11:26 -0500

    added challenge materials

---
+++ challenge_queries/Employee_Database_Challenge.sql
@@ -0,0 +1,50 @@
+-- Get titles of employees up for retirement
+SELECT e.emp_no, 
+	e.first_name, 
+	e.last_name,
+	ti.title,
+	ti.from_date,
+	ti.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as ti 
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31');
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+
+INTO unique_titles
+FROM retirement_titles
+WHERE to_date = '9999-01-01'
+ORDER BY emp_no, to_date DESC;
+
+
+-- Get the number of employees up for retirement grouped by title
+SELECT COUNT(title), title 
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
+
+-- Get the employees that are eligible for a mentorship program
+SELECT DISTINCT ON (emp_no)
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (de.emp_no = e.emp_no)
+INNER JOIN titles as ti
+ON (ti.emp_no = e.emp_no)
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
